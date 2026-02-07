b3ff2281161b530c0056ffdcc385717e70bdc1b1
reidsana <oksanareid48@gmail.com>
2026-02-07 14:08:00 -0500

    save

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -3,4 +3,7 @@ const nav = document.getElementById("primaryNav");
 
 menuButton.addEventListener("click", () => {
   nav.classList.toggle("open");
+
+  const expanded = menuButton.getAttribute("aria-expanded") === "true";
+  menuButton.setAttribute("aria-expanded", !expanded);
 });
